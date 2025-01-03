d8b686b10d87add285964d6d1d1155364ad79234
초연 <46335139+choyeon2e@users.noreply.github.com>
2025-01-03 23:30:45 +0900

    [Bronze II] Title: 단어의 개수, Time: 176 ms, Memory: 19236 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,11 +1,14 @@
-let fs = require("fs");
-let filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
 
-let input = fs.readFileSync(filePath).toString().trim().split(" ");
-let answer = 0;
+let words = input.split(' ');
+let num = 0;
 
-for (let i = 0; i < input.length; i++) {
-  if (input[i] !== "") answer++;
+for (let i=0;i<words.length; i++){
+    if (words[i] !== ''){
+        num++;
+    }
 }
 
-console.log(answer);
+console.log(num);

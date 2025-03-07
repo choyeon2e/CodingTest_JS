9081797c96c5131edf9e80d5e2c533f9b267d0b0
초연 <46335139+choyeon2e@users.noreply.github.com>
2025-03-07 15:09:01 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const num = +input[0];
+let countGroupWord = 0;
+
+for (let i=1; i<=num; i++){
+    const word = input[i];
+    const letter = [];
+    let isGroupWord = true;
+ 
+
+    for (let j=0; j<word.length; j++){
+        if (letter.indexOf(word[j])===-1){
+            letter.push(word[j]);
+        }   else{
+            if (letter.indexOf(word[j]) !== letter.length -1){
+                isGroupWord = false;
+                break;
+            }
+        }
+    }
+
+    if (isGroupWord){
+        countGroupWord += 1;
+    }
+}
+
+console.log(countGroupWord);

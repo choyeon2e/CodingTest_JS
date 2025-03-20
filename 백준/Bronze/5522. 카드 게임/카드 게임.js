d8d86c9367b4a8d17332304eaab01158d3c9a247
choyeon <46335139+choyeon2e@users.noreply.github.com>
2025-03-20 15:51:26 +0900

    [Bronze V] Title: 카드 게임, Time: 92 ms, Memory: 9336 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+console.log(input.map(Number).reduce((a,b)=>a+b,0));

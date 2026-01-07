0c0a338c8c575e8ef0b343f37e6ddb9f77a69a86
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-01-07 14:37:41 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 92 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.js
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.js
@@ -11,19 +11,6 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 let input = fs.readFileSync(filePath).toString().trim();
 
-function PrintClub(input) {
-  switch (input) {
-    case "M":
-      return "MatKor";
-    case "W":
-      return "WiCys";
-    case "C":
-      return "CyKor";
-    case "A":
-      return "AlKor";
-    case "$":
-      return "$clear";
-  }
-}
-
-console.log(PrintClub(input));
+console.log(
+  { M: "MatKor", W: "WiCys", C: "CyKor", A: "AlKor", $: "$clear" }[input]
+);

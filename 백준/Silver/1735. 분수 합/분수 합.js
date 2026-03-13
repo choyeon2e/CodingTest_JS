933e98c1e48f5a5431e3db2c2ec4bac76170e5d4
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-03-14 00:43:58 +0900

    [Silver III] Title: 분수 합, Time: 1676 ms, Memory: 9872 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.js
+++ 백준/Silver/1735. 분수 합/분수 합.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const input = fs
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
-  .trim("")
+  .trim()
   .split("\n");
 
 const [ANum, ADen] = input[0].split(" ").map(Number); //Num = 분자, Den = 분모

f014337a834770c41367b977bf711669844fe308
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-03-17 17:15:53 +0900

    [Silver IV] Title: GCD 합, Time: 224 ms, Memory: 12248 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+//최대공약수
+function getGCD(a, b) {
+  while (b !== 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+//최소공배수
+function getLCM(a, b) {
+  return (a / getGCD(a, b)) * b;
+}
+
+const t = +input[0];
+for (let i = 1; i <= t; i++) {
+  const arr = input[i].split(" ").map(Number);
+
+  const n = arr[0];
+  const numArr = arr.splice(1);
+  let sum = 0;
+
+  for (let j = 0; j < n; j++) {
+    for (let k = j + 1; k < n; k++) {
+      sum += getGCD(numArr[j], numArr[k]);
+    }
+  }
+
+  console.log(sum);
+}

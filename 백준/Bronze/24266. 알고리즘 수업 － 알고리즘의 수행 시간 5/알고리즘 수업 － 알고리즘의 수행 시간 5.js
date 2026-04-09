5b16bd56c393c81896fec05b01a46ac74c28774b
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-04-09 14:29:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = BigInt(input[0]);
+console.log((n * n * n).toString());
+console.log(3);

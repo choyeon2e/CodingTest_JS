9ca1bce4187ba1275e3b40a8663cf808db5f324e
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-04-11 04:19:41 +0900

    [Bronze V] Title: 직사각형, Time: 88 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(Number(input[0]) * Number(input[1]));

34671b9485426c928bdf3755aa9786043370ed5f
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-06-02 13:23:47 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 19.39 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -0,0 +1,30 @@
+function solution(board) {
+    let max = 0;
+    const row = board.length;
+    const col = board[0].length;
+
+    if (row < 2 || col < 2) {
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                if (board[i][j] === 1) return 1;
+            }
+        }
+        return 0;
+    }
+
+    for (let i = 1; i < row; i++) {
+        for (let j = 1; j < col; j++) {
+            if (board[i][j] === 1) {
+                board[i][j] =
+                    Math.min(
+                        board[i - 1][j],
+                        board[i][j - 1],
+                        board[i - 1][j - 1]
+                    ) + 1;
+                max = Math.max(max, board[i][j]);
+            }
+        }
+    }
+
+    return max * max;
+}

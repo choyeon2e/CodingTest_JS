27fcde92c9871bb8b5feeb340321722313ca99ef
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-07-26 02:41:27 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,12 +1,14 @@
 function solution(brown, yellow) {
     const size = brown + yellow;
 
-    for (let h = 3; h <= Math.sqrt(size); h++) {
+    for (let h = 3; h <= size; h++) {
         if (size % h === 0) {
             let w = size / h;
 
-            if ((w - 2) * (h - 2) === yellow) {
-                return [w, h];
+            if (w >= h) {
+                if ((w - 2) * (h - 2) === yellow) {
+                    return [w, h];
+                }
             }
         }
     }

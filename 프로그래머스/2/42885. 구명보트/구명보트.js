1e22368bb649c29bb4bf8532aeed2422e7ccee09
choyeon <46335139+choyeon2e@users.noreply.github.com>
2026-07-26 01:05:56 +0900

    [level 2] Title: 구명보트, Time: 14.15 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,19 +1,21 @@
+/**
+ * 구명보트는 최대 2명씩 탑승 가능. 무게제한 존재
+ * 구명보트를 최대한 적게 사용해서 모든 사람 구출하려고함
+ **/
+
 function solution(people, limit) {
     people.sort((a, b) => a - b);
-
     let left = 0;
     let right = people.length - 1;
-    let count = 0;
+    let answer = 0;
 
-    while (left < right) {
+    while (left <= right) {
         if (people[left] + people[right] <= limit) {
             left++;
         }
+
         right--;
-        count++;
+        answer++;
     }
-
-    if (left === right) count++;
-
-    return count;
+    return answer;
 }
